2af35201665e105ef7f08f0d605663ea55aa87a1
aristoteles <aristotelesandrade7@gmail.com>
2022-10-10 21:11:44 -0300

    modificado o  arquivo index.html

---
+++ src/main/java/repository/CadastroRepository.java
@@ -0,0 +1,16 @@
+package repository;
+
+
+
+import Model.Cadastro;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CadastroRepository extends JpaRepository<Cadastro, Long> {
+    
+
+
+
+}
