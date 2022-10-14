d475e62bcb11daffb44faa6536bd00897bfaf5af
alexfn93 <alexfn.dev.job@gmail.com>
2022-10-13 21:21:08 -0300

    finalizando back-end

---
+++ src/main/java/Model/Voluntarios.java
@@ -0,0 +1,39 @@
+package Model;
+
+public class Voluntarios {
+	private String codvoluntario;
+	private String Datainicio;
+	private String esco_ensino;
+	private String coddocente;
+	
+	
+	
+	//Get and Set
+	public String getCodvoluntario() {
+		return codvoluntario;
+	}
+	public void setCodvoluntario(String codvoluntario) {
+		this.codvoluntario = codvoluntario;
+	}
+	public String getDatainicio() {
+		return Datainicio;
+	}
+	public void setDatainicio(String datainicio) {
+		Datainicio = datainicio;
+	}
+	public String getEsco_ensino() {
+		return esco_ensino;
+	}
+	public void setEsco_ensino(String esco_ensino) {
+		this.esco_ensino = esco_ensino;
+	}
+	public String getCoddocente() {
+		return coddocente;
+	}
+	public void setCoddocente(String coddocente) {
+		this.coddocente = coddocente;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/example/demo/AcademyfuturoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyfuturoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademyfuturoApplication.class, args);
+	}
+
+}
